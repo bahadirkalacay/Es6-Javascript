364ef45b0bee53829a14752e559f626193a1d51e
Bahadır Kalaçay <bahadirkalacay@gmail.com>
2022-10-15 18:25:53 +0300

    güncelleme

---
+++ generators.js
@@ -0,0 +1,13 @@
+const numbers = [1,2,3,4];
+
+let total = 0 ;
+
+for (let number of numbers) {
+    total += number;
+}
+
+const colors = ["red","green","blue"]
+
+for (let color of colors) {
+    console.log(color)
+}
